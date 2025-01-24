dd22b52affb6fd8ef0765d8f45d131eaecaf7e97
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2025-01-24 17:41:30 +0500

    v1

---
+++ src/main/java/uteam_edu_crm/uteam_edu_crm/UteamEduCrmApplication.java
@@ -0,0 +1,13 @@
+package uteam_edu_crm.uteam_edu_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UteamEduCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UteamEduCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uteam_edu_crm/uteam_edu_crm/UteamEduCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package uteam_edu_crm.uteam_edu_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UteamEduCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
